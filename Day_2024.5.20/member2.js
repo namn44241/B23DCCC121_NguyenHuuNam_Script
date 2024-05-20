93fe4b6bb8b191e7c92320e54ceaf8099b3b3ff2
namn44241 <nguyenvandoaaaaa@gmail.com>
2024-05-20 10:43:48 +0700

    20.5

---
+++ Day_2024.5.20/member2.js
@@ -0,0 +1,9 @@
+const member9 = {
+    name: "Dương Minh Trí" // Sử dụng dấu hai chấm thay vì dấu bằng
+}
+
+const info = {
+    event: "New event" // Sử dụng dấu hai chấm thay vì dấu bằng
+}
+
+export { member9, info }
